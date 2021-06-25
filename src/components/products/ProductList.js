3ecc6ee9241214cc1b55f89ec0f265f4936ce168
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-25 08:40:11 +0300

    productList component connected to getProducts action at redux

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Badge } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as productActions from "../../redux/actions/productActions";
+import { Table } from "reactstrap";
 
 class ProductList extends Component {
+  componentDidMount() {
+    this.props.actions.getProducts();
+  }
+
   render() {
     return (
       <div>
         <h3>Products</h3>
-        <h5>Current category: {this.props.currentCategory.categoryName}</h5>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Unit Price</th>
+              <th>Quantity Per Unit</th>
+              <th>Units In Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
+              <tr>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitsInStock}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
@@ -16,7 +43,16 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    products: state.productListReducer,
   };
 }
 
-export default connect(mapStateToProps)(ProductList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
