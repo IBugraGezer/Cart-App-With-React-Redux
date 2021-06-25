7106f5a087db789072621fd84a99aacd4e31100f
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-25 08:49:03 +0300

    products listed by category

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,9 +4,12 @@ export function getProductsSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products };
 }
 
-export function getProducts() {
+export function getProducts(category) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
+    if (category) {
+      url += "?categoryId=" + category.id;
+    }
     return fetch(url)
       .then((response) => response.json())
       .then((data) => dispatch(getProductsSuccess(data)));
