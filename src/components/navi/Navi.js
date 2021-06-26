c8188f104f04d6bab871588846eb7d0f8dce38e9
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-26 10:34:58 +0300

    navbar edit

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -7,29 +7,27 @@ import {
   Nav,
   NavItem,
   NavLink,
-  NavbarText,
 } from "reactstrap";
 import CartSummary from "../cart/CartSummary";
+import { Link } from "react-router-dom";
 
 export default class Navi extends Component {
   render() {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand>
+            <Link to={"/"}>Cart App</Link>
+          </NavbarBrand>
           <NavbarToggler />
           <Collapse navbar>
             <Nav className="mr-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
+                <NavLink href="https://github.com/IBugraGezer/Cart-App-With-React-Redux">
                   GitHub
                 </NavLink>
               </NavItem>
             </Nav>
-            <NavbarText>Simple Text</NavbarText>
           </Collapse>
           <Nav className="ml-auto" navbar>
             <NavItem>
